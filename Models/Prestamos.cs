0f23e2c09d355e5a312097d340c50098b5912e96
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-30 18:56:28 -0400

    Modelo prestamo

---
+++ Models/Prestamos.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarcosRosario_Ap1_P1.Models
+{
+	public class Prestamos
+	{
+		[Key]
+		public int PrestamoId { get; set; }
+		[Required(ErrorMessage = "Campo obligatoria")]
+		[RegularExpression("@^[a-zA-Z]+$", ErrorMessage = "Solo letras.")]
+		public string? Nombres { get; set; }
+		[Required(ErrorMessage = "Campo obligatoria")]
+		[RegularExpression("@^[a-zA-Z]+$", ErrorMessage = "Solo letras.")]
+		public string? Concepto { get; set; }
+		[Required(ErrorMessage = "Campo obligatoria")]
+		[Range(1, double.MaxValue, ErrorMessage = "No puede ser menor a 0.")]
+		public double Monto { get; set; }
+	}
+}
