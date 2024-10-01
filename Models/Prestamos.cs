d91ac276c4d3e58d33724aad721526992100b417
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-09-30 20:53:41 -0400

    Error de Models solucionado.

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -7,10 +7,10 @@ namespace MarcosRosario_Ap1_P1.Models
 		[Key]
 		public int PrestamoId { get; set; }
 		[Required(ErrorMessage = "Campo obligatoria")]
-		[RegularExpression("@^[a-zA-Z]+$", ErrorMessage = "Solo letras.")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras.")]
 		public string? Nombres { get; set; }
 		[Required(ErrorMessage = "Campo obligatoria")]
-		[RegularExpression("@^[a-zA-Z]+$", ErrorMessage = "Solo letras.")]
+		[RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Solo letras.")]
 		public string? Concepto { get; set; }
 		[Required(ErrorMessage = "Campo obligatoria")]
 		[Range(1, double.MaxValue, ErrorMessage = "No puede ser menor a 0.")]
